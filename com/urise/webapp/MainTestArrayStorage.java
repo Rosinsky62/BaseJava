498a56028553410c80c017c1b1cc85fe9655e079
Rosinsky62 <alexander.rosinsky26@gmail.com>
2022-06-09 12:22:18 +0300

    Отредактировал класс ArrayStorage

---
+++ com/urise/webapp/MainTestArrayStorage.java
@@ -0,0 +1,50 @@
+package com.urise.webapp;
+
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.ArrayStorage;
+
+/**
+ * Test for your com.urise.webapp.storage.ArrayStorage implementation
+ */
+public class MainTestArrayStorage {
+    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+
+    public static void main(String[] args) {
+        Resume r1 = new Resume("uuid1");
+        Resume r2 = new Resume("uuid2");
+        Resume r3 = new Resume("uuid3");
+        Resume r4 = new Resume("uuid4");
+
+
+        ARRAY_STORAGE.save(r1);
+        ARRAY_STORAGE.save(r2);
+        ARRAY_STORAGE.save(r3);
+
+        System.out.println(ARRAY_STORAGE.storage[0]);
+        System.out.println(ARRAY_STORAGE.storage[1]);
+        System.out.println(ARRAY_STORAGE.storage[2]);
+
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
+        System.out.println("Size: " + ARRAY_STORAGE.size());
+        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+
+        ARRAY_STORAGE.update(r1);
+        System.out.println(r1.getUuid());
+        ARRAY_STORAGE.update(r4);
+
+        printAll();
+        ARRAY_STORAGE.delete(r1.getUuid());
+        printAll();
+        ARRAY_STORAGE.clear();
+        printAll();
+
+        System.out.println("Size: " + ARRAY_STORAGE.size());
+    }
+
+    static void printAll() {
+        System.out.println("\nGet All");
+        for (Resume r : ARRAY_STORAGE.getAll()) {
+            System.out.println(r);
+        }
+    }
+}

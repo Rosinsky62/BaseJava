a956e13cbf3a9a99e0f0b7ae7311ebec685b14ed
Rosinsky62 <alexander.rosinsky26@gmail.com>
2022-06-09 12:38:07 +0300

    Отредактировал класс ArrayStorage

--- com/urise/webapp/storage/ArrayStorage.java
+++ com/urise/webapp/storage/ArrayStorage.java
@@ -74,6 +74,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
+
         return Arrays.copyOfRange(storage, 0, size);
     }
 
